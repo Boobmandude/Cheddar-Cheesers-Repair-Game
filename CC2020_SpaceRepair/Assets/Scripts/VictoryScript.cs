dd6780e7f7285870aaea4e417116bd5cd0ac0e53
Julaxe <59695404+julaxe@users.noreply.github.com>
2020-02-02 10:28:25 -0500

    Victory and Defeat Scene

--- CC2020_SpaceRepair/Assets/Scripts/VictoryScript.cs
+++ CC2020_SpaceRepair/Assets/Scripts/VictoryScript.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour
+public class VictoryScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-         
+
     }
     public void QuitGame()
     {
-        Debug.Log("QUITTTT");
         Application.Quit();
     }
     public void Menu()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
     }
     public void Restart()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
 }

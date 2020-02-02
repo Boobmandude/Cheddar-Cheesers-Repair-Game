1df1f98174b33aaf908bebfbb3165bbadf2e8144
Boobmandude <stinsoncole@hotmail.com>
2020-02-02 03:47:46 -0500

    Spawning Shit aint working

--- CC2020_SpaceRepair/Assets/script/ObstacleController.cs
+++ CC2020_SpaceRepair/Assets/script/ObstacleController.cs
@@ -6,13 +6,73 @@ public class ObstacleController : MonoBehaviour
 {
     GameObject spawner;
     float mass;
-    float speed = 100f;
+    float speed;
+
+    public float meteorSpeed;
+    public float meteorDamage;
+    public float meteorSpeedMultiplier;
+
+    public float ufoSpeed;
+    public float ufoDamage;
+    public float ufoSpeedMultiplier;
+
+    public float shootingStarSpeed;
+    public float shootingStarDamage;
+    public float shootingStarSpeedMultiplier;
+
+    public float sataliteSpeed;
+    public float sataliteDamage;
+    public float sataliteSpeedMultiplier;
+
+    float obsatcleDamage;
+    float obstacleSpeed;
+    float obstacleSpeedMultiplier;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        if (gameObject.tag == "Meteor") {
+
+            obsatcleDamage = meteorDamage;
+            obstacleSpeed = meteorSpeed;
+            obstacleSpeedMultiplier = meteorSpeedMultiplier;
+
+           
+            print("Meteor");
+        }
+        if (gameObject.tag == "UFO")
+        {
+
+            obsatcleDamage = ufoDamage;
+            obstacleSpeed = ufoSpeed;
+            obstacleSpeedMultiplier = ufoSpeedMultiplier;
+
+        }
+        if (gameObject.tag == "ShootingStar")
+        {
+
+            obsatcleDamage = ufoDamage;
+            obstacleSpeed = ufoSpeed;
+            obstacleSpeedMultiplier = ufoSpeedMultiplier;
+
+            mass = spawner.GetComponent<ObstacleSpawner>().fMass;
+            gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(obstacleSpeed * obstacleSpeedMultiplier * Vector2.down);
+
+        }
+        if (gameObject.tag == "Satalite")
+        {
+
+            obsatcleDamage = sataliteDamage;
+            obstacleSpeed = sataliteSpeed;
+            obstacleSpeedMultiplier = sataliteSpeedMultiplier;
+
+        }
+
         spawner = GameObject.FindGameObjectWithTag("SpawnPoint");
         mass = spawner.GetComponent<ObstacleSpawner>().fMass;
-        gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(speed * 10 * Vector2.down);
+        gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(obstacleSpeed * obstacleSpeedMultiplier * Vector2.down);
+
     }
 
     // Update is called once per frame
